5e5ce2427b694731823c076dc883e9e7823ca916
pulkit2012 <pulkit.varshney_iot18@gla.ac.in>
2020-03-30 21:59:49 +0530

    final commit

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,5 +6,21 @@
  */
 package problem5.main;
 //executable class
+import problem5.student.Student;
+import problem5.circularqueue.MyCircularQueue;
+
+import java.util.ArrayList;
+
+//executable class
 public class MyMain {
+    public static void main(String[] args) {
+        ArrayList<Student> st = new ArrayList<>();
+        st.add(new Student(1,"pulkit",1,0));
+        st.add(new Student(2,"ritik",1,2));
+        st.add(new Student(3,"himanshu",3,2));
+        st.add(new Student(4,"ayush",0,0));
+        MyCircularQueue circularQueue = new MyCircularQueue();
+        circularQueue.enqueue(st);
+        circularQueue.printQueue();
+    }
 }
